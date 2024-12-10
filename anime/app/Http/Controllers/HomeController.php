dac44b1c9448463fb4ecbb3070fe02832cc16c1d
shamini nirodya <nirodyapgs.22@uom.lk>
2024-12-11 00:10:16 +0530

    displaying shows
    carousel

--- anime/app/Http/Controllers/HomeController.php
+++ anime/app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Show\Show;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $shows = Show::orderBy('id', 'desc')->take(4)->get();
+        return view('home', compact('shows'));
     }
+
 }
